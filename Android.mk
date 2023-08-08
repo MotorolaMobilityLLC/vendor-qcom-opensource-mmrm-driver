a5c1836ffc68d7301c5203e642bcc2a10808b182
Marco Zhang <quic_zhangx@quicinc.com>
2023-08-08 16:27:45 +0800

    avoid 32bit build
    Mot-CRs-Fixed: (CR)
    
    Change-Id: If9c1fb5983fbda61b3b0625b9ec88424ec366887

--- Android.mk
+++ Android.mk
@@ -21,6 +21,7 @@ include $(CLEAR_VARS)
 # For incremental compilation
 LOCAL_SRC_FILES           := $(wildcard $(LOCAL_PATH)/**/*) $(wildcard $(LOCAL_PATH)/*)
 LOCAL_MODULE              := mmrm-module-symvers
+LOCAL_MULTILIB := first
 LOCAL_MODULE_STEM         := Module.symvers
 LOCAL_MODULE_KBUILD_NAME  := Module.symvers
 LOCAL_MODULE_PATH         := $(KERNEL_MODULES_OUT)
@@ -32,6 +33,7 @@ include $(CLEAR_VARS)
 # For incremental compilation
 LOCAL_SRC_FILES           := $(wildcard $(LOCAL_PATH)/**/*) $(wildcard $(LOCAL_PATH)/*)
 LOCAL_MODULE              := msm-mmrm.ko
+LOCAL_MULTILIB := first
 LOCAL_MODULE_KBUILD_NAME  := driver/msm-mmrm.ko
 LOCAL_MODULE_TAGS         := optional
 LOCAL_MODULE_DEBUG_ENABLE := true
